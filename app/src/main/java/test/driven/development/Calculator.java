cc8a729995f3831b79bab914bcc14254e526fea5
Aditya <adityadudhal97@gmail.com>
2025-01-11 13:19:03 +0530

    Implemented add method to handle empty string input

--- app/src/main/java/test/driven/development/Calculator.java
+++ app/src/main/java/test/driven/development/Calculator.java
@@ -2,6 +2,9 @@ package test.driven.development;
 
 public class Calculator {
     public int add(String numbers) {
+        if (numbers.length() == 0) {
+            return 0;
+        }
         return 999;
     }
 }
