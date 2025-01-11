1ad57e02af6a059779f1a34456d7e92772cee91f
Aditya <adityadudhal97@gmail.com>
2025-01-11 13:55:57 +0530

    Added test for negative numbers exception

--- app/src/test/java/test/driven/development/CalculatorTest.java
+++ app/src/test/java/test/driven/development/CalculatorTest.java
@@ -6,32 +6,38 @@ import org.junit.Test;
 public class CalculatorTest {
 
     @Test
-    public void testAddForEmptyInput(){
+    public void testAddForEmptyInput() {
         Calculator calculator = new Calculator();
         Assert.assertEquals(0, calculator.add(""));
     }
 
     @Test
-    public void testAddForSingleNumber(){
+    public void testAddForSingleNumber() {
         Calculator calculator = new Calculator();
         Assert.assertEquals(1, calculator.add("1"));
     }
 
     @Test
-    public void testAddForTwoNumber(){
+    public void testAddForTwoNumber() {
         Calculator calculator = new Calculator();
         Assert.assertEquals(6, calculator.add("1,5"));
     }
 
     @Test
-    public void testAddForNumbersLineAndCommaSeparated(){
+    public void testAddForNumbersLineAndCommaSeparated() {
         Calculator calculator = new Calculator();
         Assert.assertEquals(6, calculator.add("1\n2,3"));
     }
 
     @Test
-    public void testAddForNumbersCustomSeparated(){
+    public void testAddForNumbersCustomSeparated() {
         Calculator calculator = new Calculator();
         Assert.assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void testAddForNegativeNumbers() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals("negative numbers not allowed -1", calculator.add("-1"));
     }
 }
